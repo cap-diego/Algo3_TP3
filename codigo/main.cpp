1bbd21823e1e242fda3382a5109dc062baf8ddf8
cap-diego <diegobuceta35@gmail.com>
2018-11-02 09:48:37 -0300

    primer commit

---
+++ codigo/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
+
+
+//Metodo de clustering:
+//Me paro en un nodo cualquiera y voy a sus k vecinos más cercanos.
